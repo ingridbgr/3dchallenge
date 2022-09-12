03dc063ca9f3e6d3344a988b1fe505e9477e9fda
ingrid_bgr <ingrid.rodrigues@purespectrum.com>
2022-09-12 00:20:55 -0300

    added the navigation and info panel

---
+++ src/components/SectionInfo.tsx
@@ -0,0 +1,44 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownToggle from "react-bootstrap/DropdownToggle";
+import DropdownMenu from "react-bootstrap/DropdownMenu";
+
+import { userInfo } from "../mocks/mocks";
+
+const SectionInfo = () => {
+	return (
+		<section className="info-panel">
+			<div className="button-area">
+				<button className="btn-primary">
+					<span className="material-symbols-outlined">arrow_back_ios</span>
+					return
+				</button>
+				<button className="btn-primary">
+					<span className="material-symbols-outlined"> visibility</span>preview
+				</button>
+			</div>
+			<div className="patient-info">
+				<div className="patient-data">{userInfo.patient.id}</div>
+				<div className="patient-data">
+					<span className="material-symbols-outlined">medical_information</span>
+					{userInfo.user}
+				</div>
+                    <div className="patient-data">
+					<span className="material-symbols-outlined">local_hospital</span>
+					{userInfo.patient.hospital}
+				</div>
+                <div className="patient-data">
+					<span className="material-symbols-outlined">personal_injury</span>
+					{userInfo.patient.name}
+				</div>
+                <div className="patient-data">
+					<span className="material-symbols-outlined">calendar_month</span>
+					{userInfo.patient.date}
+				</div>
+			</div>
+            <button className="btn-primary">Tools
+            <span className="material-symbols-outlined">expand_more</span>
+			</button>
+		</section>
+	);
+};
+export default SectionInfo;
---
+++ src/mocks/mocks.tsx
@@ -0,0 +1,12 @@
+export const userInfo = {
+    user: 'Dr. Brenner',
+    admin: true, 
+    badge: 3,
+    patient: {
+        id: '#568769',
+        doctor: 'Dr. Brenner',
+        hospital: 'Clemont Hospital',
+        name: 'Jane Doe', 
+        date: '14/12/22',
+    }
+}
