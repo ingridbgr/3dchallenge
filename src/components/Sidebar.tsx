4cb2cc038b3d1aee33d63728523c4bc2d3e43111
ingrid_bgr <ingrid.rodrigues@purespectrum.com>
2022-09-14 01:32:11 -0300

    refactored sidebar dropdowns

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,8 +3,9 @@ interface ValueProps {
 	setIsOpen: (value: any) => void;
 }
 
-const Sidebar = ({ isOpen, setIsOpen}: ValueProps) => {
+const Sidebar = ({ isOpen, setIsOpen }: ValueProps) => {
 	let dropdown: any = document.getElementsByClassName("dropdown-btn");
+	let dropdownContainer: any = document.getElementsByClassName("dropdown-container");
 	let i;
 
 	for (i = 0; i < dropdown.length; i++) {
@@ -13,25 +14,42 @@ const Sidebar = ({ isOpen, setIsOpen}: ValueProps) => {
 			let dropdownContent = this.nextElementSibling;
 			if (dropdownContent.style.display === "flex") {
 				dropdownContent.style.display = "none";
+			} else if (dropdownContent.style.display === "flex") {
 			} else {
-				dropdownContent.style.display = "flex"
+				dropdownContent.style.display = "flex";
 			}
 		});
 	}
-	const handleClick = () => isOpen ? setIsOpen(false):  setIsOpen(true)
+	const handleClick = () => {
+		if (isOpen) {
+			setIsOpen(false);
+			dropdownContainer.style.display = "none";
+		}else {
+		setIsOpen(true);
+		}
+	};
 	return (
 		<div className={!isOpen ? "nav sidebar" : "nav sidebar expanded"}>
 			<ul className="nav-list">
 				<li className="mb-3">
 					<button
 						className="btn mx-2 btn-sidebar d-flex sidebar-heading "
-						onClick={handleClick}>
-						{isOpen ? <span className="material-symbols-outlined">chevron_left</span> : ''}
-							Tasks
-						{!isOpen ? <span className="material-symbols-outlined">chevron_right</span> : ''}
+						onClick={handleClick}
+					>
+						{isOpen ? (
+							<span className="material-symbols-outlined">chevron_left</span>
+						) : (
+							""
+						)}
+						Tasks
+						{!isOpen ? (
+							<span className="material-symbols-outlined">chevron_right</span>
+						) : (
+							""
+						)}
 					</button>
 				</li>
-				<li className="nav-list-item" >
+				<li className="nav-list-item">
 					<button className="btn btn-sidebar dropdown-btn">
 						<span className="material-symbols-outlined">check_circle</span>
 						<span className="badge badge-pill badge-info">1</span>
@@ -41,13 +59,14 @@ const Sidebar = ({ isOpen, setIsOpen}: ValueProps) => {
 						</span>
 					</button>
 					<div className="dropdown-container">
-						<a className="btn-secondary tasks" href="#">Task 1</a>
-						
+						<a className="  tasks" href="#">
+							Task 1
+						</a>
 					</div>
 				</li>
-				<li className="nav-list-item" >
+				<li className="nav-list-item">
 					<button className="btn btn-sidebar dropdown-btn">
-					<span className="material-symbols-outlined">task</span>
+						<span className="material-symbols-outlined">task</span>
 						<span className="badge badge-pill badge-info">4</span>
 						<span className="sidebar-title">
 							Current Tasks
@@ -55,15 +74,23 @@ const Sidebar = ({ isOpen, setIsOpen}: ValueProps) => {
 						</span>
 					</button>
 					<div className="dropdown-container">
-					<a className="btn-secondary tasks" href="#">Task 1</a>
-						<a className="btn-secondary tasks" href="#">Task 2</a>
-						<a className="btn-secondary tasks" href="#">Task 3</a>
-						<a className="btn-secondary tasks" href="#">Task 4</a>
+						<a className="  tasks" href="#">
+							Task 1
+						</a>
+						<a className="  tasks" href="#">
+							Task 2
+						</a>
+						<a className="  tasks" href="#">
+							Task 3
+						</a>
+						<a className="  tasks" href="#">
+							Task 4
+						</a>
 					</div>
 				</li>
 				<li className="nav-list-item">
 					<button className="btn btn-sidebar dropdown-btn">
-					<span className="material-symbols-outlined">calendar_month</span>
+						<span className="material-symbols-outlined">calendar_month</span>
 						<span className="badge badge-pill badge-info">3</span>
 						<span className="sidebar-title">
 							Upcoming Tasks
@@ -71,10 +98,15 @@ const Sidebar = ({ isOpen, setIsOpen}: ValueProps) => {
 						</span>
 					</button>
 					<div className="dropdown-container">
-					<a className="btn-secondary tasks" href="#">Task 1</a>
-						<a className="btn-secondary tasks" href="#">Task 2</a>
-						<a className="btn-secondary tasks" href="#">Task 3</a>
-
+						<a className="  tasks" href="#">
+							Task 1
+						</a>
+						<a className="  tasks" href="#">
+							Task 2
+						</a>
+						<a className="  tasks" href="#">
+							Task 3
+						</a>
 					</div>
 				</li>
 			</ul>
